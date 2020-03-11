b98287d99bc16d5f0f36668ab90a04e50dbc3086
abhishek-kumar251095 <abhishek.kumar251095@gmail.com>
2020-03-12 01:44:20 +0530

    Added separate module for journals.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,30 +7,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { JournalMainComponent } from './journal-main/journal-main.component';
-import { EntryAddComponent } from './journal-main/entry-add/entry-add.component';
-import { EntriesComponent } from './journal-main/entries/entries.component';
-import { EntryDisplayComponent } from './journal-main/entry-display/entry-display.component';
-import { EntriesDisplayComponent } from './journal-main/entries/entries-display/entries-display.component';
 
-import { ShortenPipe } from '../app/pipes/shorten.pipe';
+import { JournalMainModule } from './journal-main/journal-main.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    JournalMainComponent,
-    EntryAddComponent,
-    EntriesComponent,
-    EntryDisplayComponent,
-    EntriesDisplayComponent,
-    ShortenPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    JournalMainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
