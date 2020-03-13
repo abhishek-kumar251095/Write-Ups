bded66b4ccc0f8a4fa75c8cbf88b0e7d208762ab
abhishek-kumar251095 <abhishek.kumar251095@gmail.com>
2020-03-14 00:04:18 +0530

    Animation on empty timeline alert.

--- src/app/timeline-home/timeline-home.component.ts
+++ src/app/timeline-home/timeline-home.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit, Input, AfterViewChecked, AfterViewInit } from '@angular/core';
 import {TimelineModel} from '../Models-Shared/timeline-model';
 import { TimelineService } from '../services/timeline.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
  
 @Component({
   selector: 'app-timeline-home',
   templateUrl: './timeline-home.component.html',
-  styleUrls: ['./timeline-home.component.css']
+  styleUrls: ['./timeline-home.component.css'],
+  animations:[
+    trigger('emptyState', [
+      // state('in', style({
+      //   'opacity': 1,
+      //   'transform':'translatex(0)'
+      // })),
+      transition('void => *', [
+        style({
+          'opacity': 0,
+          'transform': 'translatex(-100px)'
+        }),
+        animate(500)
+      ])
+    ])
+  ]
 })
 export class TimelineHomeComponent implements OnInit {
 
