1b8d448671a1e4b47e1bec0764b350e6be1fe684
abhishek-kumar251095 <abhishek.kumar251095@gmail.com>
2020-03-14 20:44:36 +0530

    Modified UI on Add-Entry page.

--- src/app/journal-main/entry-add/entry-add.component.ts
+++ src/app/journal-main/entry-add/entry-add.component.ts
@@ -3,21 +3,44 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormArray } fr
 import {EntryModel} from '../Models/entry-model';
 import {JournalService} from '../../services/journal.service';
 import { TimelineModel } from 'src/app/Models-Shared/timeline-model';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-entry-add',
   templateUrl: './entry-add.component.html',
-  styleUrls: ['./entry-add.component.css']
+  styleUrls: ['./entry-add.component.css'],
+  animations:[
+    trigger('tagState', [
+      transition('void => *', [
+        style({
+          'opacity':0,
+          'transform':'translatey(100px)'
+        }),
+        animate(200)
+      ])
+    ])
+  ]
 })
 export class EntryAddComponent implements OnInit {
 
-  constructor(private journalService: JournalService) { }
   journalEntry: FormGroup
+  tagInputCount: number = 0
+
+  constructor(private journalService: JournalService) { }
 
   ngOnInit(): void {
 
+    const currentDateTime = new Date();
+    const dateOptions = {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric"   
+    };
+
     this.journalEntry = new FormGroup({
-      dateTime: new FormControl(new Date()),
+      dateTime: new FormControl(currentDateTime.toLocaleString("en", dateOptions)),
       title: new FormControl(null, Validators.required),
       content: new FormControl(null, Validators.required),
       tags: new FormArray([])
@@ -32,8 +55,11 @@ export class EntryAddComponent implements OnInit {
       content: this.journalEntry.value.content,
       length: this.journalEntry.value.content.length,
       tags: this.journalEntry.value.tags
-    }
-    
+    };
+
+    const filteredEntry = entry.tags.filter(item => item != null);
+    entry.tags = filteredEntry;
+
     this.journalService.addEntry(entry);
     (<FormArray>this.journalEntry.get('tags')).clear();
     this.journalEntry.reset();
@@ -43,11 +69,21 @@ export class EntryAddComponent implements OnInit {
   }
 
   onAddTag(){
-    const control = new FormControl(null,Validators.required);
-    (<FormArray>this.journalEntry.get('tags')).push(control);
+
+    if (this.tagInputCount < 6) {
+      const control = new FormControl(null,Validators.required);
+      (<FormArray>this.journalEntry.get('tags')).push(control);
+      this.tagInputCount += 1;
+    }
+
   }
 
   getControls(){
     return (<FormArray>this.journalEntry.get('tags')).controls;
+  }
+
+  onInput(){
+    const element = document.getElementById("content");
+    element.style.height = element.style.height = element.scrollHeight + "px";
   }
 }
